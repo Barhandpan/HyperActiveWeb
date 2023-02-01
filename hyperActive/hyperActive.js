07934b7087fef296b18cadcf052a8ff5625573da
barhandpan <barhandpan@gmail.com>
2023-02-01 17:20:10 +0200

    final

--- hyperActive/hyperActive.js
+++ hyperActive/hyperActive.js
@@ -1,13 +1,11 @@
-addEventListeners()
-doOtherThings()
 
-
-
-const categoryContainer = document.querySelectorAll('.category-container')
-const titleContainer = document.querySelectorAll('.title-container')
-
+// const categoryContainer = document.querySelectorAll('.category-container')
+// const titleContainer = document.querySelectorAll('.title-container')
+// addEventListeners1()
 // create function
-categoryContainer.forEach((element,index) => {
+function showReverseCategory(container, container2)
+{
+    container.forEach((element,index) => {
     element.addEventListener("click", (event)=>{
         if(element.children[1].classList.contains("show")) {
             element.children[1].classList.remove("show");
@@ -16,71 +14,80 @@ categoryContainer.forEach((element,index) => {
             element.children[1].classList.remove("reverse");
             element.children[1].classList.add("show");
         }
-    titleContainer[index].children[2].classList.contains('exit')?titleContainer[index].children[2].classList.remove('exit'):titleContainer[index].children[2].classList.add('exit')
+        container2[index].children[2].classList.contains('exit')?container2[index].children[2].classList.remove('exit'):container2[index].children[2].classList.add('exit')
 })})
-
-const  addEventListeners1=()=>{
-    el1()
-    el2()
-    el3()
 }
 
-function addEventListeners2(){
-    el1()
-    el2()
-    el3()
-}
 
-const questionContainer = document.querySelectorAll('.question-answer-container')
-const answer = document.querySelectorAll('.answer')
-const question = document.querySelectorAll('.question')
-questionContainer.forEach((element,index) => {
-    element.addEventListener("click",() =>{
-if(answer[index].classList.contains("show")) {
-            answer[index].classList.remove("show");
-            answer[index].classList.add("reverse");
-        } else {
-            answer[index].classList.remove("reverse");
-            answer[index].classList.add("show");
-        }
-        question[index].children[0].classList.contains('fa-circle-arrow-down')?question[index].children[0].className = "fa-solid fa-circle-arrow-up":question[index].children[0].className = "fa-solid fa-circle-arrow-down"
+// const  addEventListeners1=()=>{
+showReverseCategory(document.querySelectorAll('.category-container'),document.querySelectorAll('.title-container'))
+showReverseAnswer(document.querySelectorAll('.question-answer-container'),document.querySelectorAll('.answer'),document.querySelectorAll('.question'))
+openMenu(document.getElementById('menu-button'),document.querySelector('.nav-links-container'),document.getElementById('header'),false)
+showReverseCareers(window.matchMedia("(max-width: 925px)"),document.getElementById('careers'),document.getElementById('inner-careers'),document.getElementById('nav-link-id'),false)
+// }
+
+// const questionContainer = document.querySelectorAll('.question-answer-container')
+// const answer = document.querySelectorAll('.answer')
+// const question = document.querySelectorAll('.question')
+function showReverseAnswer(container,answer,question){
+    container.forEach((element,index) => {
+        element.addEventListener("click",() =>{
+    if(answer[index].classList.contains("show")) {
+                answer[index].classList.remove("show");
+                answer[index].classList.add("reverse");
+            } else {
+                answer[index].classList.remove("reverse");
+                answer[index].classList.add("show");
+            }
+            question[index].children[0].classList.contains('fa-circle-arrow-down')?question[index].children[0].className = "fa-solid fa-circle-arrow-up":question[index].children[0].className = "fa-solid fa-circle-arrow-down"
+        })
     })
-})
-
+}
 const menuBtn = document.getElementById('menu-button')
 const links = document.querySelector('.nav-links-container')
 let menuOpen = false;
 let headerContainer = document.getElementById('header');
-
-menuBtn.addEventListener('click', ()=>{
-    menuBtn.classList.contains('open')?menuBtn.classList.remove('open'):menuBtn.classList.add('open')
-    links.classList.contains('show-links')?links.classList.remove('show-links'):links.classList.add('show-links')
-    menuOpen === false?menuOpen = true:menuOpen = false;
-    if(menuOpen)
-        headerContainer.style.height = `${headerContainer.getBoundingClientRect().height + links.getBoundingClientRect().height}px`;
-    else
-        headerContainer.style.height = '53px'
-})
-
+function openMenu(menuBtn,links,headerContainer,menuOpen){
+    menuBtn.addEventListener('click', ()=>{
+        menuBtn.classList.contains('open')?menuBtn.classList.remove('open'):menuBtn.classList.add('open')
+        links.classList.contains('show-links')?links.classList.remove('show-links'):links.classList.add('show-links')
+        menuOpen === false?menuOpen = true:menuOpen = false;
+        if(menuOpen)
+            headerContainer.style.height = `${headerContainer.getBoundingClientRect().height + links.getBoundingClientRect().height}px`;
+        else
+        {
+            headerContainer.style.height = '53px'
+            innerCareers.classList.remove("open-careers")
+            CareersMenuOpen = false
+        }
+    })
+}
 const mq = window.matchMedia("(max-width: 925px)");
 const careers = document.getElementById('careers')
 const innerCareers = document.getElementById('inner-careers')
+const navLink = document.getElementById('nav-link-id')
 let CareersMenuOpen = false;
+function showReverseCareers(mq,careers,innerCareers,navLink,CareersMenuOpen){
     careers.addEventListener('click', () =>{
         if(mq.matches)
         {
-            innerCareers.classList.toggle("open-careers");
+            innerCareers.classList.contains("open-careers")?innerCareers.classList.remove("open-careers"):innerCareers.classList.add("open-careers")
             CareersMenuOpen === false?CareersMenuOpen = true:CareersMenuOpen = false;
             if(CareersMenuOpen)
-            headerContainer.style.height = `${headerContainer.getBoundingClientRect().height + innerCareers.getBoundingClientRect().height}px`;
-        else
+            headerContainer.style.height = `${headerContainer.getBoundingClientRect().height + careers.getBoundingClientRect().height}px`;
+        else{
             headerContainer.style.height = '558px';
+            navLink.style.height = '483px'
+        }
         }
 });
-
-
 if(mq.matches)
+{
     innerCareers.style.display === 'none';
+    navLink.style.height = '483px'
+}
+}
+
 const sumbitContactBtn = document.getElementById('submit-contact-container')
 const submitDesktop = document.getElementById('submit-desktop')
 var regex = /^[a-zA-Z\s]+$/;
